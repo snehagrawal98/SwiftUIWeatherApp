9c50f2e84b3e212b1a05ab80059d0a8a5863f0a5
Sneha Agrawal <snehagrawal98@gmail.com>
2021-12-10 03:12:11 +0530

    Initial Commit

---
+++ SwiftUIWeatherApp/SwiftUIWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherAppApp.swift
+//  SwiftUIWeatherApp
+//
+//  Created by mnameit on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
